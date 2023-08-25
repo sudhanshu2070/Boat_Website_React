4984ed97f1ad245a31731ffc3f25da6a39bded0e
sudhanshu2070 <sssallen2016@gmail.com>
2023-08-25 21:43:36 +0530

    updated file

--- src/HeroText.tsx
+++ src/HeroText.tsx
@@ -19,9 +19,9 @@ function HeroText() {
         className="flex flex-col text-3vw <md:text-24px font-bold mb-30px pt-100px"
       >
         <span className="flex items-center gap-10px">
-          delivering <Compass /> industry's
+          Vemy <Compass /> Salvatore
         </span>
-        <span>top tallents work</span>
+        <span>cruising around</span>
       </motion.div>
       <motion.span
         variants={reveal}
@@ -30,9 +30,7 @@ function HeroText() {
         transition={{ delay: animationStart + 1.2, duration: 0.5 }}
         className="mb-30px w-1/4 <lg:w-1/3 <md:w-1/2 text-14px leading-tight"
       >
-        vr headset warable network effect. hypergrowth activist investor.
-        substack blogging thiel fellow dropout. alexis ohanian tweeted overhyped
-        ed-tech series.
+       You put the thing that does the killing right between your teeth, but you never give it the power to kill you
       </motion.span>
     </motion.div>
   );
